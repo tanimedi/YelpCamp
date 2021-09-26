985866357158e92c0519f3a45531bd93825f0bb3
Tanisha <tjmedi87@gmail.com>
2021-09-26 18:44:47 -0400

    add form for saving new campground

--- app.js
+++ app.js
@@ -18,16 +18,27 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req,res)=>{res.render('home')})
 app.get('/campgrounds', async(req,res)=>{
    const campgrounds= await Campground.find({});
    res.render('campgrounds/index',{campgrounds})
 })
 
+app.get('/campgrounds/new',(req,res)=>{
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds',async(req,res)=>{
+    res.send(req.body);
+})
+
 app.get('/campgrounds/:id', async(req, res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground});
 })
+
 
 
 app.listen(3000,()=>{console.log('Serving on port 3000')})
