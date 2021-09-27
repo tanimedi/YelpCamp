bc0a9f63c7cebf0a1ad89835169f35a8a16ff19c
Tanisha <tjmedi87@gmail.com>
2021-09-27 18:39:15 -0400

    start campground edit page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const mongoose = require('mongoose');
 const path =require('path');
+const methodOverride=require('method-override');
 const Campground=require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 app.get('/', (req,res)=>{res.render('home')})
 app.get('/campgrounds', async(req,res)=>{
@@ -31,14 +33,23 @@ app.get('/campgrounds/new',(req,res)=>{
 })
 
 app.post('/campgrounds',async(req,res)=>{
-    res.send(req.body);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.get('/campgrounds/:id', async(req, res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground});
+});
+
+app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit',{campground});
 })
 
-
+app.put('/campgrounds/:id', async(req, res)=>{
+    res.send("IT WORKED!!")
+})
 
 app.listen(3000,()=>{console.log('Serving on port 3000')})
