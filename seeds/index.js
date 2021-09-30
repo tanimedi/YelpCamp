8c580ed400b5c9c2459691a3e83db576dbca7838
Tanisha <tjmedi87@gmail.com>
2021-09-30 19:54:25 -0400

    add pictures, prices, descriptions, style all campgrounds page

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB=async()=>{
     await Campground.deleteMany({});
     for(let i=0; i<50; i++){
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*20)+10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem odio iusto ut nulla ducimus saepe hic quisquam tenetur consequuntur! Odio dolore adipisci ullam neque assumenda illo doloremque autem magni amet.',
+            price
         })
         await camp.save();
     }
