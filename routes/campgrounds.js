497b093036be1dd104a9f191050c6c993b5960af
Tanisha <tjmedi87@gmail.com>
2021-10-11 00:10:11 -0400

    configure session, add flash, start flash partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -32,9 +32,9 @@ router.get(
     validateCampground,
     catchAsync(async (req, res, next) => {
       // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
-  
       const campground = new Campground(req.body.campground);
       await campground.save();
+      req.flash('success','Successfully made a new campground!');
       res.redirect(`/campgrounds/${campground._id}`);
     })
   );
@@ -46,7 +46,7 @@ router.get(
         "reviews"
       );
       console.log(campground);
-      res.render("campgrounds/show", { campground });
+      res.render("campgrounds/show", { campground});
     })
   );
   
@@ -66,6 +66,7 @@ router.get(
       const campground = await Campground.findByIdAndUpdate(id, {
         ...req.body.campground
       });
+      req.flash('success','Successfully updated campground!');
       res.redirect(`/campgrounds/${campground._id}`);
     })
   );
--- routes/reviews.js
+++ routes/reviews.js
@@ -28,6 +28,7 @@ router.post(
       campground.reviews.push(review);
       await review.save();
       await campground.save();
+      req.flash('success', 'Created new review!');
       res.redirect(`/campgrounds/${campground._id}`);
     })
   );
