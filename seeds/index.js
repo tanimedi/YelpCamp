e7f9f9693fe7b3e0b1311a0f332c14cbb3890aaf
tanimedi <tjmedi87@gmail.com>
2021-10-14 18:29:23 -0400

    initial commit

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -32,7 +32,10 @@ const seedDB = async () => {
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem odio iusto ut nulla ducimus saepe hic quisquam tenetur consequuntur! Odio dolore adipisci ullam neque assumenda illo doloremque autem magni amet.",
       price,
-      geometry:{ type: 'Point', coordinates: [ -113.1331, 47.0202 ] },
+      geometry: {
+        type: "Point",
+        coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+      },
       images: [
         {
           url:
